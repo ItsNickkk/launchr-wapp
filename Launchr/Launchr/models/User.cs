22d0b1f4281535156f4312884c3c80aee4df5030
JZ <ooijingzhi@gmail.com>
2020-04-22 19:46:45 +0800

    initial commit

---
+++ Launchr/Launchr/models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Launchr.models
+{
+    public class User
+    {
+        public User(string id, string name, string address, string phone_number, string email, string country, int status, int is_admin, string username, string password)
+        {
+
+        }
+    }
+}
