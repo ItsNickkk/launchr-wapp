9bfb0d2d3e6b7c4421e789f59e1256da60de5731
JZ <ooijingzhi@gmail.com>
2020-04-22 20:54:23 +0800

    basic btn click

--- Launchr/Launchr/pages/login.aspx.cs
+++ Launchr/Launchr/pages/login.aspx.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Data;
+using Launchr.models;
 
 namespace Launchr.pages
 {
@@ -12,11 +13,21 @@ namespace Launchr.pages
 	{
 		protected void Page_Load(object sender, EventArgs e)
 		{
-		
+			
 		}
 
 		protected void btnLogin_Click(object sender, EventArgs e)
 		{
+			SiteDB siteDB = new SiteDB();
+			List<User> user_list = siteDB.getUserByUsernameAndPassword(this.txtMemberLoginUsername.Text, this.txtMemberLoginPasssword.Text);
+			if(user_list.Count() == 0)
+			{
+				Console.WriteLine("No User Found.");
+			} else if (user_list.Count() == 1)
+			{
+				this.txtMemberLoginUsername.Text = "Success!";
+				Console.WriteLine(user_list[0].ToString());
+			}
 
 		}
 	}
