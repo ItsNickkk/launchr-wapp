9768dea4c001c488f9a1f956859b9add99d929a0
JZ <ooijingzhi@gmail.com>
2020-05-07 18:22:14 +0800

    do not show currently logged in admin

--- Launchr/Launchr/pages/ban-user-admin.aspx.cs
+++ Launchr/Launchr/pages/ban-user-admin.aspx.cs
@@ -22,15 +22,20 @@ namespace Launchr.pages
 		private void makeUserList(List<User> user_list)
 		{
 			StringBuilder html = new StringBuilder();
+			User current_user = (User)this.Session["user"];
 			foreach(User user in user_list)
 			{
-				if(user.status == 1)
+				if(user.id != current_user.id)
 				{
-					html.Append("<tr><th scope=\"row\" class=\"text-right\">" + user.id + "</th><td>" + user.name + "</td><td>" + user.email + "</td><td>" + user.username + "</td><td class=\"text-center\"><button type=\"button\" class=\"btnBanUser launchr-btn btn p-2 btnBanUser\">Ban User</button></td></tr>");
-				} else
-				{
-					html.Append("<tr><th scope=\"row\" class=\"text-right\">" + user.id + "</th><td>" + user.name + "</td><td>" + user.email + "</td><td>" + user.username + "</td><td class=\"text-center\"><button type=\"button\" class=\"btnUnbanUser launchr-btn btn p-2 btnUnbanUser\">Unban User</button></td></tr>");
+					if(user.status == 1)
+					{
+						html.Append("<tr><th scope=\"row\" class=\"text-right\">" + user.id + "</th><td>" + user.name + "</td><td>" + user.email + "</td><td>" + user.username + "</td><td class=\"text-center\"><button type=\"button\" class=\"btnBanUser launchr-btn btn p-2 btnBanUser\">Ban User</button></td></tr>");
+					} else
+					{
+						html.Append("<tr><th scope=\"row\" class=\"text-right\">" + user.id + "</th><td>" + user.name + "</td><td>" + user.email + "</td><td>" + user.username + "</td><td class=\"text-center\"><button type=\"button\" class=\"btnUnbanUser launchr-btn btn p-2 btnUnbanUser\">Unban User</button></td></tr>");
+					}
 				}
+				
 			}
 
 			this.plcUserList.Controls.Add(new Literal
@@ -107,7 +112,17 @@ namespace Launchr.pages
 		[WebMethod]
 		public static int unbanCreator(object userID)
 		{
-			return 1; //return 1 for successful data manipulation
+			int creator_id = int.Parse((string)userID);
+			Creator creator = new SiteDB().getCreatorById(creator_id);
+			if (creator != null)
+			{
+				creator.status = 1;
+				return creator.update();
+			}
+			else
+			{
+				return 0;
+			}
 		}
 	}
 }
