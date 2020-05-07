f9d66985b315ed97c1d52b6a107717d2fb7a74aa
ItsNickkk <48113405+ItsNickkk@users.noreply.github.com>
2020-05-07 15:56:10 +0800

    ban user basic done

---
+++ Launchr/Launchr/pages/ban-user-admin.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Launchr.pages
+{
+	public partial class ban_user_admin : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		[WebMethod]
+		public static int banMember(object userID)
+		{
+			return 1; //return 1 for successful data manipulation
+		}
+
+		[WebMethod]
+		public static int unbanMember(object userID)
+		{
+			return 1; //return 1 for successful data manipulation
+		}
+
+		[WebMethod]
+		public static int banCreator(object userID)
+		{
+			return 1; //return 1 for successful data manipulation
+		}
+
+		[WebMethod]
+		public static int unbanCreator(object userID)
+		{
+			return 1; //return 1 for successful data manipulation
+		}
+	}
+}
